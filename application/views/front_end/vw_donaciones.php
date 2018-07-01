cd14f5aafada7ab472bdcae0948632f6d5c46f0c
TheRoggerUTL <36090167+TheRogger@users.noreply.github.com>
2018-06-30 22:29:46 -0500

    Se agrego una animación en wv_donaciones

--- application/views/front_end/vw_donaciones.php
+++ application/views/front_end/vw_donaciones.php
@@ -23,7 +23,7 @@
 
 		<div style="text-align: center;">	
 
-			<h1>Donación por Paypal, tarjeta de credito o debito</h1>
+			<h1 class="animated infinite shake">Donación por Paypal, tarjeta de credito o debito</h1>
 				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 				<input type="hidden" name="cmd" value="_s-xclick">
 				<input type="hidden" name="hosted_button_id" value="Z732G64LEN49G">
